66a8c16aaf5e6d7f87fbdbfe9ca04f9bf23cc0c3
Tais <55287175+RocketBadger@users.noreply.github.com>
2020-03-24 13:31:34 +0100

    Controller create metoder skrevet

--- AarhusBryghus/src/controller/Controller.java
+++ AarhusBryghus/src/controller/Controller.java
@@ -33,8 +33,32 @@ public class Controller {
 		return storage.getAllProduktGrupper();
 	}
 
-	// TODO
-	// OpretProdukt
-	// OpretProduktGruppe
-	// createSomeObjects to have some startup objects
+	public Produkt createProdukt(String navn, String nr, ProduktGruppe produktGruppe) {
+		try {
+			Produkt p = new Produkt(navn, nr, produktGruppe);
+			produktGruppe.addProdukt(p);
+			storage.addProdukt(p);
+			return p;
+		} catch (IllegalArgumentException i) {
+			System.out.println("ProduktGruppe muligvis ikke gyldig");
+			System.out.println("Message: " + i);
+			return null;
+		}
+	}
+
+	public ProduktGruppe createProduktGruppe(String type, String beskrivelse) {
+		try {
+			ProduktGruppe pg = new ProduktGruppe(type, beskrivelse);
+			storage.addProduktGruppe(pg);
+			return pg;
+		} catch (IllegalArgumentException i) {
+			System.out.println("Message: " + i);
+			return null;
+		}
+	}
+
+	public void createSomeObjects() {
+		this.createProduktGruppe("Øl", "Det øl");
+		this.createProdukt("Klosterbryg", "1", storage.getAllProduktGrupper().get(0));
+	}
 }
