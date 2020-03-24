881834931fa7809114301d9f3a1663fa6c2f4990
RocketBadger <55287175+RocketBadger@users.noreply.github.com>
2020-03-24 13:09:15 +0100

    Model/Storage klasser oprettet, basal controller oprettet

--- AarhusBryghus/src/controller/Controller.java
+++ AarhusBryghus/src/controller/Controller.java
@@ -1,5 +1,40 @@
 package controller;
 
-public class Controller {
+import java.util.List;
 
+import model.Produkt;
+import model.ProduktGruppe;
+import storage.Storage;
+
+public class Controller {
+	private Storage storage;
+	private static Controller controller;
+
+	private Controller() {
+		storage = new Storage();
+	}
+
+	public static Controller getController() {
+		if (controller == null) {
+			controller = new Controller();
+		}
+		return controller;
+	}
+
+	public static Controller getTestController() {
+		return new Controller();
+	}
+
+	public List<Produkt> getAllProdukter() {
+		return storage.getAllProdukter();
+	}
+
+	public List<ProduktGruppe> getAllProduktGrupper() {
+		return storage.getAllProduktGrupper();
+	}
+
+	// TODO
+	// OpretProdukt
+	// OpretProduktGruppe
+	// createSomeObjects to have some startup objects
 }
--- AarhusBryghus/src/storage/Storage.java
+++ AarhusBryghus/src/storage/Storage.java
@@ -1,5 +1,37 @@
 package storage;
 
-public class Storage {
+import java.util.ArrayList;
+import java.util.List;
 
+import model.Produkt;
+import model.ProduktGruppe;
+
+public class Storage {
+	private List<Produkt> produkter;
+	private List<ProduktGruppe> produktGrupper;
+
+	public Storage() {
+		produkter = new ArrayList<Produkt>();
+		produktGrupper = new ArrayList<ProduktGruppe>();
+	}
+
+	public void addProdukt(Produkt produkt) {
+		if (!produkter.contains(produkt)) {
+			produkter.add(produkt);
+		}
+	}
+
+	public List<Produkt> getAllProdukter() {
+		return new ArrayList<>(produkter);
+	}
+
+	public void addProduktGruppe(ProduktGruppe produktGruppe) {
+		if (!produktGrupper.contains(produktGruppe)) {
+			produktGrupper.add(produktGruppe);
+		}
+	}
+
+	public List<ProduktGruppe> getAllProduktGrupper() {
+		return new ArrayList<>(produktGrupper);
+	}
 }
