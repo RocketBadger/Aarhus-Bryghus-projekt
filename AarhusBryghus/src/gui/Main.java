d02aa3f1b57d861ac50ab275a962c65fd77f1421
OTJacobsen <37661636+OTJacobsen@users.noreply.github.com>
2020-04-04 12:14:35 +0200

    Ny gui ændringer

--- AarhusBryghus/src/gui/Main.java
+++ AarhusBryghus/src/gui/Main.java
@@ -1,30 +1,21 @@
 package gui;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 import controller.Controller;
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	private Controller controller;
-	Stage window;
-	Scene scene;
-	Scene salgScene;
-	Scene produktScene;
-	Scene prisScene;
-	Scene oversigtScene;
 
 	public static void main(String[] args) {
 		Application.launch(args);
@@ -38,98 +29,63 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		window = stage;
 		stage.setTitle("Aarhus Bryghus Kasseapparat");
 		BorderPane pane = new BorderPane();
 		this.initContent(pane);
 
-		scene = new Scene(pane, 700, 600);
+		Scene scene = new Scene(pane, 1000, 700);
 		pane.setId("pane");
 		scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
 		stage.setScene(scene);
 		stage.setResizable(false);
 		stage.show();
+		stage.setTitle("Sidebar test");
+		
+		stage.setScene(scene);
+		stage.show();
 
 	}
 
-	private void initContent(BorderPane pane) throws FileNotFoundException {
+	private void initContent(BorderPane pane) {
 		File saveFile = new File("src/storage.ser");
 		if (saveFile.exists()) {
 			controller.loadStorage();
 		}
-		VBox vbx = new VBox();
-		vbx.setPadding(new Insets(20, 0, 20, 0));
-		vbx.setSpacing(30);
-
-		Image headerLogo = new Image(new FileInputStream("src/gui/ABLOGO.png"));
-		ImageView imgView = new ImageView(headerLogo);
-		imgView.setFitHeight(150);
-		imgView.setFitWidth(320);
-
-		Button btn1 = new Button("Udfør Salg");
-		btn1.setId("button");
-		Button btn2 = new Button("Produkter og produktgrupper");
-		Button btn3 = new Button("Prislister og priser");
-		Button btn4 = new Button("Oversigter");
-		btn2.setId("button");
-		btn3.setId("button");
-		btn4.setId("button");
-		vbx.getChildren().addAll(imgView, btn1, btn2, btn3, btn4);
-
-		vbx.setAlignment(Pos.TOP_CENTER);
-		pane.setCenter(vbx);
-
-		btn1.setOnAction(e -> salgGui());
-		btn2.setOnAction(e -> produktGui());
-		btn3.setOnAction(e -> prisGui());
-		btn4.setOnAction(e -> oversigtGui());
+		
+		SalgsPane salgspane = new SalgsPane();
+		ProduktPane produktpane = new ProduktPane();
+		PrisPane prispane = new PrisPane();
+		OversigtPane oversigtspane = new OversigtPane();
+		pane.setCenter(salgspane);
+		
+		
+		// ---------- Side menu start ----------
+		DropShadow dropshadow = new DropShadow(BlurType.GAUSSIAN, Color.color(0.25, 0.25, 0.25, 0.15), 2, 5, 5, 0);
+		VBox vbxButtons = new VBox();
+		vbxButtons.setEffect(dropshadow);
+		vbxButtons.setPadding(new Insets(50, 20, 50, 20));
+		vbxButtons.setSpacing(50);
+		vbxButtons.setStyle("-fx-background-color: #a7adb4");
+		
+		Button btnSalg = new Button("Udfør salg");
+		btnSalg.setId("MainButton");
+		btnSalg.setOnAction(e -> pane.setCenter(salgspane));
+		
+		Button btnProdukt = new Button("Produkter og produktgrupper");
+		btnProdukt.setId("MainButton");
+		btnProdukt.setOnAction(e -> pane.setCenter(produktpane));		
+		
+		Button btnPris = new Button("Prislister og priser");
+		btnPris.setId("MainButton");
+		btnPris.setOnAction(e -> pane.setCenter(prispane));
+		
+		Button btnOversigt = new Button("Oversigter");
+		btnOversigt.setId("MainButton");
+		btnOversigt.setOnAction(e -> pane.setCenter(oversigtspane));
+		
+		vbxButtons.getChildren().addAll(btnSalg, btnProdukt, btnPris, btnOversigt);
+		pane.setLeft(vbxButtons);
+		// ---------- Side menu slut ----------
 
 	}
-
-	public void salgGui() {
-		SalgGui salg = new SalgGui(window, scene);
-
-		GridPane pane = new GridPane();
-		pane.add(salg, 0, 0);
-
-		salgScene = new Scene(pane, 725, 600);
-		pane.setId("salgGui");
-		salgScene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-		window.setScene(salgScene);
-	}
-
-	public void produktGui() {
-		ProduktGui produkt = new ProduktGui(window, scene);
-
-		GridPane pane = new GridPane();
-		pane.add(produkt, 0, 0);
-
-		produktScene = new Scene(pane, 600, 500);
-		pane.setId("produktGui");
-		produktScene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-		window.setScene(produktScene);
-	}
-
-	public void prisGui() {
-		PrisGui pris = new PrisGui(window, scene);
-
-		GridPane pane = new GridPane();
-		pane.add(pris, 0, 0);
-
-		prisScene = new Scene(pane, 625, 500);
-		pane.setId("prisGui");
-		prisScene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-		window.setScene(prisScene);
-	}
-
-	public void oversigtGui() {
-		OversigtPane oversigter = new OversigtPane(window, scene);
-		GridPane pane = new GridPane();
-		pane.add(oversigter, 0, 0);
-		oversigtScene = new Scene(pane, 960, 450);
-		pane.setId("oversigtGui");
-		oversigtScene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
-		window.setScene(oversigtScene);
-	}
-
 }
--- AarhusBryghus/src/gui/OversigtPane.java
+++ AarhusBryghus/src/gui/OversigtPane.java
@@ -2,19 +2,21 @@ package gui;
 
 import controller.Controller;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
 public class OversigtPane extends GridPane {
 	private Controller controller;
+	private Button btnVisSalgPaaDag = new Button("Vis Salg");
 
-	public OversigtPane(Stage stage, Scene scene) {
+	public OversigtPane() {
 		this.controller = Controller.getController();
 
 		this.setPadding(new Insets(20));
 		this.setHgap(20);
 		this.setVgap(10);
 		this.setGridLinesVisible(false);
+		
+		this.add(btnVisSalgPaaDag, 0, 0);
 	}
 }
